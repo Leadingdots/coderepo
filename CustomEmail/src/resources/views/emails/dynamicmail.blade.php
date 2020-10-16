1b40471c462960a68161c66cc93ece3b33f6cad0
ARVIND KUMAR MAURYA <maurya.arvind70@gmail.com>
2020-10-16 10:52:48 +0530

    removed pacakage

--- CustomEmail/src/resources/views/emails/dynamicmail.blade.php
+++
@@ -1,6 +0,0 @@
-@php
-foreach($tokens as $key => $token){
-    $template = str_replace('#'.$key.'#',$token,$template);
-}
-@endphp
-{!!$template!!}
--- CustomEmail/src/resources/views/inc/pagination.blade.php
+++
@@ -1,4 +0,0 @@
-
-@if(count($result))
-    {!!$result->links('customemail::inc.paginator')!!}
-@endif
